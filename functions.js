924ac2bf88a4c2bba7579f293e5ea5922f98ffdf
Shahid Amin CYF <82290321+Schboostie@users.noreply.github.com>
2023-04-22 16:41:49 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -6,7 +6,7 @@ function displayUsers() {
     if (Object.hasOwnProperty.call(users, key)) {
       const user = users[key];
       usersHtml += `
-        <div>
+        <div class="user-card">
           <h2>${user.firstName} ${user.lastName}</h2>
           <p>Age: ${user.age}</p>
           <p>Email: ${user.email}</p>
@@ -19,3 +19,4 @@ function displayUsers() {
 }
 
 displayUsers();
+
