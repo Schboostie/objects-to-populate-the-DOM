71d1628333a47a51eb1c5b9ef2ea4cf67fefa4f7
Shahid Amin CYF <82290321+Schboostie@users.noreply.github.com>
2023-04-27 16:52:12 +0100

    Create fun-disney.js

---
+++ fun-disney.js
@@ -0,0 +1,37 @@
+const disneyCharacters = [
+  {
+    name: "Mickey Mouse",
+    species: "Mouse",
+    color: "Black",
+    friends: ["Donald Duck", "Goofy", "Minnie Mouse"],
+    enemies: ["Pete"],
+  },
+  {
+    name: "Donald Duck",
+    species: "Duck",
+    color: "White",
+    friends: ["Mickey Mouse", "Goofy", "Daisy Duck"],
+    enemies: ["Chip 'n' Dale"],
+  },
+  {
+    name: "Goofy",
+    species: "Dog",
+    color: "Brown",
+    friends: ["Mickey Mouse", "Donald Duck", "Pluto"],
+    enemies: ["Pete"],
+  },
+  {
+    name: "Simba",
+    species: "Lion",
+    color: "Golden Brown",
+    friends: ["Timon", "Pumbaa", "Nala"],
+    enemies: ["Scar", "Shenzi", "Banzai", "Ed"],
+  },
+  {
+    name: "Ariel",
+    species: "Mermaid",
+    color: "Red",
+    friends: ["Flounder", "Sebastian", "Prince Eric"],
+    enemies: ["Ursula"],
+  },
+];
